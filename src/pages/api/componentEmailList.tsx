ac5475b189dc17fc04e02da79c0f4f62b73b2fd0
Stanlys <stanlys@yandex.ru>
2023-04-13 16:32:27 +0300

    feat: correct emails list

--- src/pages/api/componentEmailList.tsx
+++ src/pages/api/componentEmailList.tsx
@@ -1,24 +1,56 @@
-import React, { useState } from "react";
-import { Button, Modal, Divider, Tree, Space, Typography, Input, Switch, Table } from "antd";
-const { Text, Link } = Typography;
+import React, { ChangeEvent, useState } from "react";
+import { Button, Modal, Divider, Tree, Space, Typography, Input } from "antd";
+const { Text } = Typography;
 
 const ComponentEmailList = () => {
-    const [emailList, setEmailList] = useState<string[]>([]);
+    const [emailList, setEmailList] = useState<string[]>([""]);
 
     const addEmail = () => {
         setEmailList((emailList) => [...emailList, ""]);
     };
 
+    const delEmail = (index: number) => {
+        setEmailList((emailList) => emailList.filter((_, i) => i !== index));
+    };
+
+    const changeEmail = (e: ChangeEvent<HTMLInputElement>, index: number) => {
+        setEmailList((emails) => {
+            let copy = Object.assign([], emails) as string[];
+            copy[index] = e.target.value;
+            return copy;
+        });
+    };
+
     return (
         <Space direction="vertical" style={{ width: "30rem" }}>
-            <Text>Email</Text>
-            <Input size="large" />
-            <Divider></Divider>
-            {emailList.map((email, index) => (
+            {emailList.length === 1 ? (
+                <>
+                    <Text>Email</Text>
+                    <Input size="large" value={emailList[0]} />
+                    <Divider></Divider>
+                </>
+            ) : (
+                emailList.map((email, index) => (
+                    <div key={`email${index}`}>
+                        <div style={{ display: "flex", justifyContent: "space-between" }}>
+                            <Text>Email</Text>
+                            <Button danger type="dashed" onClick={() => delEmail(index)}>
+                                - Удалить email
+                            </Button>
+                        </div>
+
+                        <Input size="large" value={emailList[index]} onChange={(e) => changeEmail(e, index)} />
+
+                        <Divider></Divider>
+                    </div>
+                ))
+            )}
+
+            {/* {emailList.map((email, index) => (
                 <>
                     <div style={{ display: "flex", justifyContent: "space-between" }}>
                         <Text>Email</Text>
-                        <Button danger onClick={() => console.log("delete", index)}>
+                        <Button danger type="dashed" onClick={() => delEmail(index)}>
                             - Удалить email
                         </Button>
                     </div>
@@ -27,7 +59,7 @@ const ComponentEmailList = () => {
 
                     <Divider></Divider>
                 </>
-            ))}
+            ))} */}
             <Button type="dashed" block onClick={addEmail}>
                 + Добавить еще email
             </Button>
