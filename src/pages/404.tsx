1a2fea1187a414c8f2782ef0bf51cab3c37286e4
Stanlys <stanlys@yandex.ru>
2023-04-19 16:20:16 +0300

    fix: add 404

---
+++ src/pages/404.tsx
@@ -0,0 +1,11 @@
+import styles from "./../styles/NotForund.module.css";
+
+export const Custom404 = () => {
+    return (
+        <div className={styles.wrapper}>
+            <h1>404 - Page Not Found</h1>;
+        </div>
+    );
+};
+
+export default Custom404;
